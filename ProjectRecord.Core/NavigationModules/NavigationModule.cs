6263dc6cc3e07a5d8558f0842cd47d9fd5d6426c
XIOA <1327916255@qq.com>
2025-12-09 08:57:16 +0800

    Module

---
+++ ProjectRecord.Core/NavigationModules/NavigationModule.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProjectRecord.Common.Attribute;
+using ProjectRecord.Common.Services;
+
+namespace NavigationModules;
+
+[Module(nameof(NavigationModule))]
+public class NavigationModule : AutoModule<NavigationModule>
+{
+    public override void Register(IServiceCollection serviceCollection)
+    {
+        base.Register(serviceCollection);
+    }
+}
